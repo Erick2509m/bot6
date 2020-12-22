e3f72402783f61f479ade5bd9d4c9f4db931932e
Erick2509m <76112486+Erick2509m@users.noreply.github.com>
2020-12-22 14:11:34 -0500

    Add files via upload

---
+++ bot-modular-discordjs-v12-master/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+ dirBase : './database/base_1.db',
+ superusers: [], //Puedes añadir ID
+ token: 'NzgzMTExNzUwNDc1NTc5NDA0.X8V_pg.UA71oFvLAwN1v3Lyb2hpF9cVlD4',
+ ytKey: 'AIzaSyB93AnGyP5BtwxYEJc-On_HLWLvDhqVD38',
+ prefix: 'r!',
+ statusBOT: 'Estoy listo!',
+ categories: [
+  {name: "general", priority: 5},
+  {name: "admin", priority: 8},
+  {name: "music", priority: 7}
+ ],
+ groups: [
+  {name: "User", permlvl: 0},
+  {name: "Member", permlvl: 1},
+  {name: "Mod", permlvl: 2},
+  {name: "Admin", permlvl: 3}
+ ]
+ 
+
+}
